c0e22b4262711d8e895885ca2e827b868acd6c5b
Rahul Krishna <rahulkrishna.web@gmail.com>
2025-10-15 05:10:08 +0530

    make light theme default

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.className} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light">
           {/* Page Transition Wrapper */}
           <Navbar />
           {children}
