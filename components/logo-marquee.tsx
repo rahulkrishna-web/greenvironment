af70775f7e4f3d12aee046fdd013169a92581827
Rahul Krishna <rahulkrishna.web@gmail.com>
2025-10-20 05:16:50 +0530

    refactor logo marquee component for improved readability

--- components/logo-marquee.tsx
+++ components/logo-marquee.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'; 
 import {
   Marquee,
   MarqueeContent,
@@ -8,30 +8,30 @@ import {
 import { Logo01, Logo02, Logo03, Logo04, Logo05, Logo06, Logo07, Logo08 } from './logos';
 
 const logos = [
-    <Logo01 />,
-    <Logo02 />,
-    <Logo03 />,
-    <Logo04 />,
-    <Logo05 />,
-    <Logo06 />,
-    <Logo07 />,
-    <Logo08 />,
-]
+  <Logo01 />,
+  <Logo02 />,
+  <Logo03 />,
+  <Logo04 />,
+  <Logo05 />,
+  <Logo06 />,
+  <Logo07 />,
+  <Logo08 />,
+];
+
 const LogoMarquee = () => (
-  <div className="flex size-full py-8 items-center justify-center bg-background">
+  <div className="flex w-full py-8 items-center justify-center bg-background">
     <Marquee>
       <MarqueeFade side="left" />
       <MarqueeFade side="right" />
       <MarqueeContent>
         {logos.map((logo, index) => (
-            <div key={index}>
-          <MarqueeItem className="h-auto w-auto">
+          <MarqueeItem key={index} className="h-auto w-auto">
             {logo}
           </MarqueeItem>
-          </div>
         ))}
       </MarqueeContent>
     </Marquee>
   </div>
 );
+
 export default LogoMarquee;
